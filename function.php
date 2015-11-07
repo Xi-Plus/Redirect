3e1bb8d3632a35f8caaef0bb4383a7a02348152e
Xi-Plus <huangxuanyuxiplus@gmail.com>
2015-11-07 15:41:01 +0800

    use function

---
+++ function.php
@@ -0,0 +1,19 @@
+<?php
+function get_redirect_url($url){
+	require(__DIR__."/config.php");
+	$rurl="";
+
+	if(preg_match("/(\w+).tnfsh.tn.edu.tw\/files\/(\d+)-(\d+)-(\d+)-(\d+).php/",$url,$match))$rurl=$config["redirect_url"]."?w=tnfsh&q=".$match[1].".".$match[2].".".$match[3].".".$match[4].".".$match[5];
+	else if(preg_match("/www.tngs.tn.edu.tw\/tngs\/board\/index.asp\?numid=(\d+)/",$url,$match))$rurl=$config["redirect_url"]."?w=tngs&q=".$match[1];
+
+	$res=new stdClass;
+	if($rurl!=""){
+		$res->success=true;
+		$res->url=urlencode($rurl);
+	}else {
+		$res->success=false;
+		$res->url=urlencode($url);
+	}
+	return $res;
+}
+?>
